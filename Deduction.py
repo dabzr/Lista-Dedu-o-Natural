edb41368337981260ec10fad47b5dc736e1b6782
dabzr <dylanab@pm.me>
2025-02-18 08:04:37 -0300

    Adicionando Questão 3 e 4

--- Deduction.py
+++ Deduction.py
@@ -42,7 +42,7 @@ def _(mo):
     3.    { AxP(x)       hip
     4.      P(a)         Ae 3
     5.      P(a) | Q(y)  |i 4
-            }
+          }
     6.    { Q(y)         hip
     7.      P(a) | Q(y)  |i 6
           }
@@ -50,7 +50,7 @@ def _(mo):
     }
     9. Ax(P(x) | Q(y)) Ai 2-8
     """
-    mo.md(q1)
+    mo.show_code()
     return (q1,)
 
 
@@ -97,14 +97,94 @@ def _(mo):
        }
     17. AxP(x) | Q(y)      raa 2-16
     """
-    mo.md(q2)
-
+    mo.show_code()
     return (q2,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(check_proof, mo, q2):
     mo.md(check_proof(q2, display_fitch=False, display_gentzen=False))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        Questão 3
+
+        ∃xP(x) ∨ ∃xQ(x) ⊢ ∃x(P(x) ∨ Q(x))
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    q3 = """
+    1. ExP(x) | ExQ(x)     pre
+    2. { ExP(x)            hip
+    3.   { a P(a)          hip
+    4.     P(a) | Q(a)     |i 3
+    5.     Ex(P(x) | Q(x)) Ei 4
+         }
+    6.   Ex(P(x) | Q(x))   Ee 2, 3-5
+       }
+    7. { ExQ(x)            hip
+    8.   {a Q(a)           hip
+    9.    P(a) | Q(a)      |i 8
+    10.   Ex(P(x) | Q(x))  Ei 9
+         }
+    11.  Ex(P(x) | Q(x))   Ee 7, 8-10
+       }
+    12. Ex(P(x) | Q(x))    |e 1, 2-6, 7-11
+    """
+    mo.show_code()
+    return (q3,)
+
+
+@app.cell(hide_code=True)
+def _(check_proof, mo, q3):
+    mo.md(check_proof(q3, display_fitch=False, display_gentzen=False))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        Questão 4
+
+        ∃x(P(x) ∨ Q(x)) ⊢ ∃xP(x) ∨ ∃xQ(x)
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    q4 = """
+    1. Ex(P(x) | Q(x))      pre
+    2. { a P(a) | Q(a)      hip
+    3.   {P(a)              hip
+    4.    ExP(x)            Ei 3
+    5.    ExP(x) | ExQ(x)   |i 4
+         }
+    6.   {Q(a)              hip
+    7.    ExQ(x)            Ei 6
+    8.    ExP(x) | ExQ(x)   |i 7
+         }
+    9.   ExP(x) | ExQ(x)    |e 2, 3-5, 6-8
+      }
+    10. ExP(x) | ExQ(x)     Ee 1, 2-9
+    """
+    mo.show_code()
+    return (q4,)
+
+
+@app.cell(hide_code=True)
+def _(check_proof, mo, q4):
+    mo.md(check_proof(q4, display_fitch=False, display_gentzen=False))
     return
 
 
